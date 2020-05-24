a3587000a9d0fdcb280b82ce2222788bfc6a1422
Nguyen Thanh Nam <thanhnam192@gmail.com>
2020-05-24 22:05:37 +0700

    change folder sturcture
    change folder sturcture

--- my-first-app/src/containers/App.js
+++ my-first-app/src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
 // import styled from 'styled-components';
 import classes from './App.css';
 
